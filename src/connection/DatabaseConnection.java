248441c94d94959c5c14ebf44e3e381e90644041
Lucas <lucaskvn@outlook.com>
2017-10-17 09:16:43 +0200

    Added jdbc connection

--- src/connection/DatabaseConnection.java
+++ src/connection/DatabaseConnection.java
@@ -1,5 +1,57 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+	
+	public Connection getConnection() {
+		System.out.println("-------- PostgreSQL "
+				+ "JDBC Connection Testing ------------");
+
+		try {
+
+			Class.forName("org.postgresql.Driver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your PostgreSQL JDBC Driver? "
+					+ "Include in your library path!");
+			e.printStackTrace();
+			return null;
+
+		}
+
+		System.out.println("PostgreSQL JDBC Driver Registered!");
+
+		Connection connection = null;
+
+		try {
+
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/pizza", "postgres",
+					"postgres");
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return connection;
+
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+			return connection;
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+		return connection;
+	}
+	
+	public static void main(String[] args) {
+		new DatabaseConnection().getConnection();
+	}
 
 }
